747047e7fa30663b93cfb21e4d2b6825c4ccec29
Daniel T <dan.tiganas@gmail.com>
2025-01-19 15:21:25 +0200

    added new shell to vsk

---
+++ vsk/shell_noasm.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+	char command[255];
+	for (;;) {
+		write(1, "# ", 2);
+		int count = read(0, command, 255);
+		// /bin/ls\n -> /bin/ls\0
+		command[count - 1] = 0;
+		pid_t fork_result = fork();
+		if (fork_result == 0) {
+			execve(command, 0, 0);
+			break;
+		} else {
+			// wait
+			// 
+			siginfo_t info;
+			waitid(P_ALL, 0, &info, WEXITED);
+		}
+	}
+
+	_exit(0);
+}
