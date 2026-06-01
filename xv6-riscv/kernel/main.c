4c01d6e924eaf957ac082a201ea9f26a26e987e4
duron750 <dan.tiganas@gmail.com>
2026-06-01 22:53:57 +0300

    added unix version 4

--- xv6-riscv/kernel/main.c
+++ xv6-riscv/kernel/main.c
@@ -18,17 +18,24 @@ main()
     printf("\n");
     printf("hart 0 starting\n");
     kinit();         // physical page allocator
+    printf("initializing virtual memory\n");
     kvminit();       // create kernel page table
     kvminithart();   // turn on paging
+    printf("initializing process table\n");
     procinit();      // process table
+    printf("initializing trap vectors\n");
     trapinit();      // trap vectors
     trapinithart();  // install kernel trap vector
+    printf("setting up interrupt controller\n");
     plicinit();      // set up interrupt controller
     plicinithart();  // ask PLIC for device interrupts
     binit();         // buffer cache
+    printf("configuring inode and file table\n");
     iinit();         // inode table
     fileinit();      // file table
+    printf("initializing hard drive\n");
     virtio_disk_init(); // emulated hard disk
+    printf("starting init...\n");
     userinit();      // first user process
     __sync_synchronize();
     started = 1;
